372ea939d2057d300f0a266fef67506f23161074
Sébastien Levert <slevert@outlook.com>
2022-03-16 14:52:28 -0400

    Fixing Prettier

--- samples/mgt-teams-channel-messages/src/mgt-teams-channel-messages.js
+++ samples/mgt-teams-channel-messages/src/mgt-teams-channel-messages.js
@@ -1,34 +1,34 @@
-import { TemplateHelper } from "@microsoft/mgt"
+import { TemplateHelper } from "@microsoft/mgt";
 
-const channelPicker = document.querySelector("mgt-teams-channel-picker")
-const channelMessages = document.getElementById("mgt-teams-channel-messages")
+const channelPicker = document.querySelector("mgt-teams-channel-picker");
+const channelMessages = document.getElementById("mgt-teams-channel-messages");
 
-TemplateHelper.globalContext.currentTeamId = ""
-TemplateHelper.globalContext.currentChannelId = ""
+TemplateHelper.globalContext.currentTeamId = "";
+TemplateHelper.globalContext.currentChannelId = "";
 
 TemplateHelper.globalContext.formatDate = date => {
-  let messageDate = new Date(date)
-  let locale = "en-US"
+  let messageDate = new Date(date);
+  let locale = "en-US";
 
   // Formatter for "Today" and "Yesterday" etc
   const relative = new Intl.RelativeTimeFormat(locale, {
     numeric: "always",
     hour: "numeric",
     minute: "numeric"
-  })
+  });
 
   // Formatter for "Today" and "Yesterday" times
   const relativeTime = new Intl.DateTimeFormat(locale, {
     hour: "numeric",
     minute: "numeric"
-  })
+  });
 
   // Formatter for weekdays, e.g. "Monday"
   const short = new Intl.DateTimeFormat(locale, {
     weekday: "long",
     hour: "numeric",
     minute: "numeric"
-  })
+  });
 
   // Formatter for dates, e.g. "Mon, 31 May 2021"
   const long = new Intl.DateTimeFormat(locale, {
@@ -37,39 +37,39 @@ TemplateHelper.globalContext.formatDate = date => {
     year: "numeric",
     hour: "numeric",
     minute: "numeric"
-  })
+  });
 
-  let formattedDate = "nothing"
+  let formattedDate = "nothing";
 
-  console.log(formattedDate)
+  console.log(formattedDate);
 
-  const now = new Date().setHours(0, 0, 0, 0)
-  const then = new Date(messageDate).setHours(0, 0, 0, 0)
-  const days = Math.ceil((then - now) / 86400000)
+  const now = new Date().setHours(0, 0, 0, 0);
+  const then = new Date(messageDate).setHours(0, 0, 0, 0);
+  const days = Math.ceil((then - now) / 86400000);
 
-  console.log(days)
+  console.log(days);
 
   if (days > -6) {
     if (days > -2) {
       return `${relative.format(days, "day")}, ${relativeTime.format(
         messageDate
-      )}`
+      )}`;
     }
-    return short.format(messageDate)
+    return short.format(messageDate);
   }
-  return long.format(messageDate)
-}
+  return long.format(messageDate);
+};
 
 channelPicker.addEventListener("selectionChanged", e => {
   if (e.detail.length) {
-    let channelId = e.detail[0].channel.id
-    let teamId = e.detail[0].team.id
-    TemplateHelper.globalContext.currentTeamId = teamId
-    TemplateHelper.globalContext.currentChannelId = channelId
-    channelMessages.resource = `teams/${teamId}/channels/${channelId}/messages/delta`
-    channelMessages.refresh(true)
-    channelMessages.style.display = "block"
+    let channelId = e.detail[0].channel.id;
+    let teamId = e.detail[0].team.id;
+    TemplateHelper.globalContext.currentTeamId = teamId;
+    TemplateHelper.globalContext.currentChannelId = channelId;
+    channelMessages.resource = `teams/${teamId}/channels/${channelId}/messages/delta`;
+    channelMessages.refresh(true);
+    channelMessages.style.display = "block";
   } else {
-    channelMessages.style.display = "none"
+    channelMessages.style.display = "none";
   }
-})
+});
