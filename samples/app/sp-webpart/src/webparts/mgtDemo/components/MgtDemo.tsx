412df99be9cb7700eee66ec793021204def22cd6
Sébastien Levert <sebastienlevert@users.noreply.github.com>
2024-01-16 12:49:21 -0500

    Removing the confusion with mgt-react and disambiguation (#13)

--- samples/app/sp-webpart/src/webparts/mgtDemo/components/MgtDemo.tsx
+++ samples/app/sp-webpart/src/webparts/mgtDemo/components/MgtDemo.tsx
@@ -1,23 +1,16 @@
 import * as React from 'react';
 import { IMgtDemoProps } from './IMgtDemoProps';
 
-// to ensure your web part is leveraging the shared mgt library,
-// make sure you are only importing from
-//  - @microsoft/mgt-react/dist/es6/spfx
-//  - @microsoft/mgt-spfx
-// and no other mgt packages
-// If you wish to make used of element disambiguation you should:
-// configure disambiguation at the root component while lazy loading all
-// components that import from @microsoft/mgt-react
 import {
   Person,
   People,
   Agenda,
   TeamsChannelPicker,
   Tasks,
-  PeoplePicker
-} from '@microsoft/mgt-react/dist/es6/generated/react';
-import { PersonViewType, PersonCardInteraction } from '@microsoft/mgt-react/dist/es6/index';
+  PeoplePicker,
+  PersonViewType,
+  PersonCardInteraction
+} from '@microsoft/mgt-react';
 
 export default class MgtDemo extends React.Component<IMgtDemoProps, Record<string, unknown>> {
   public render(): React.ReactElement<IMgtDemoProps> {
